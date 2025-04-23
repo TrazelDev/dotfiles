4d967bd885ddf9d2df46503df274b078d572b649
TrazelDev <nirmot327@gmail.com>
2025-04-23 15:29:30 +0300

    Added nvim config that is a lazyvim distro with close to no modification, some tweaks needs to be done in the future

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.expandtab = false   -- use real tabs, not spaces
+vim.opt.tabstop = 4         -- how many spaces a tab visually equals
+vim.opt.shiftwidth = 4      -- indentation width for << and >>
+vim.opt.softtabstop = 4     -- how many spaces a tab feels like when editing
