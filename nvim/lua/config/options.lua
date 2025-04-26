f3de5e1984306ad40b0fd2ca33937acca1c6414f
TrazelDev <nirmot327@gmail.com>
2025-04-26 22:01:33 +0300

    (feature)[nvim]: Added color column to know where lines are too long

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,7 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Tabs in your code:
 vim.opt.expandtab = false   -- use real tabs, not spaces
 vim.opt.tabstop = 4         -- how many spaces a tab visually equals
 vim.opt.shiftwidth = 4      -- indentation width for << and >>
 vim.opt.softtabstop = 4     -- how many spaces a tab feels like when editing
+
+-- Line for how long can your code get:
+vim.opt.colorcolumn = "80"
