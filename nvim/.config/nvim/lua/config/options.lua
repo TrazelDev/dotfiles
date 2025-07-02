e2df73cb27e347b4263422312a6baf6b96149808
TrazelDev <nirmot327@gmail.com>
2025-07-02 18:02:33 +0300

    (feature)[nvim]: Added treesitter plugin to have highlighting
    * It was originally not there cause I had tab issues.
    * Now the tab is a regular tab still

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,10 +3,12 @@
 -- Add any additional options here
 
 -- Tabs in your code:
-vim.opt.expandtab = false   -- use real tabs, not spaces
-vim.opt.tabstop = 4         -- how many spaces a tab visually equals
-vim.opt.shiftwidth = 4      -- indentation width for << and >>
-vim.opt.softtabstop = 4     -- how many spaces a tab feels like when editing
+vim.opt.expandtab = false -- use real tabs, not spaces
+vim.opt.tabstop = 4 -- how many spaces a tab visually equals
+vim.opt.shiftwidth = 4 -- indentation width for << and >>
+vim.opt.softtabstop = 4 -- how many spaces a tab feels like when editing
+vim.opt.autoindent = true -- Automatically indent new lines
+vim.opt.smartindent = true -- Smart indentation for C-like languages (optional, but good)
 
 -- Line for how long can your code get:
 vim.opt.colorcolumn = "80"
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,21 +1,12 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    highlight = { enable = false }, -- Set enable to false for highlighting
-    indent = { enable = false },    -- Set enable to false for indentation
-    autotag = { enable = false },   -- Set enable to false for autotag (if you use it)
-    context_commentstring = { enable = false }, -- Disable other features if present
-    incremental_selection = { enable = false },
-    textobjects = { enable = false },
-    -- Keep ensure_installed if you want parsers to be available later,
-    -- but they won't be used if features are disabled.
+    highlight = { enable = false },
+    indent = { enable = false },
+
     ensure_installed = {
-      "c", -- You can keep this, it just won't be active
-      -- ... other languages
+      "c",
+      "python",
     },
-    -- Remove or comment out any specific language disables if you disable globally
-    -- disable = { "c" }, -- This is not needed if you disable globally
-    -- ... other treesitter options
   },
-  -- ... other plugin options
 }
