8ea4cc6e4f979d6b2ce35cba3e87a17e157cc1cd
Nir Kriemer <nirmot327@gmail.com>
2026-02-14 13:56:12 +0200

    (fix)[hyprland/scripts]: Make KillActiveProcess script work correctly

--- hypr/.config/hypr/scripts/KillActiveProcess.sh
+++ hypr/.config/hypr/scripts/KillActiveProcess.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
 
-# Copied from Discord post. Thanks to @Zorg
+active_pid=$(hyprctl activewindow -j | jq '.pid')
 
+if [ -z "$active_pid" ] || [ "$active_pid" -eq 0 ]; then
+    exit 1
+fi
 
-# Get id of an active window
-active_pid=$(hyprctl activewindow | grep -o 'pid: [0-9]*' | cut -d' ' -f2)
-
-# Close active window
-kill $active_pid
+kill -9 -"$active_pid" 2>/dev/null || kill -9 "$active_pid"
